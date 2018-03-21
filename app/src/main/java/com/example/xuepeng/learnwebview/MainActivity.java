ec8295800233b346d492356ca4be9be3a90fc4d2
xuepeng <helloxuepeng@gmail.com>
2018-03-22 01:00:51 +0800

    请求权限实例

--- app/src/main/java/com/example/xuepeng/learnwebview/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/learnwebview/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.xuepeng.learnwebview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wv=findViewById(R.id.wv);
+        wv.loadUrl("http://www.swordman.wang");
     }
 }
